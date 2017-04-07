301d5e16d68e19ddb9f7890a31658a80bbc414d4
hm <borth.rony@gmail.com>
2017-04-07 21:29:44 +0530

    node server added

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express = require('express');
+var express = require('express');
 const path = require('path');
-const app = express();
+//Create our app
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+	if(req.headers['x-forwarded-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
+	}else {
+		next();
+	}
+});
 
 app.use(express.static('./build'));
 
@@ -8,4 +19,6 @@ app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, './build', 'index.html'));
  });
 
-app.listen(9000);
+app.listen(PORT, function(){
+	console.log('Express server is up on port' + PORT);
+});
